beca160e4d4fb031149cfddefceb8ec24bd99b18
davididev <davidwaggener@gmail.com>
2024-03-23 13:23:03 -0400

    Sparks appear now
    I found the issue.  You forgot to add "Player" physics layer to the fall zone 2 collision mask.
    I had to comment out "reload_scene" for it to show up.

--- Scenes/FallZone2.gd
+++ Scenes/FallZone2.gd
@@ -4,11 +4,11 @@ var particle_instance = null
 #var scene_loader = SceneLoader.new()
 func _on_body_entered(_body):
 	pass
-	#if particle_instance == null:
-		#print("This worked. Wheres my sparks???")
-		#var instance = particle_scene
-		#add_child(instance)
-		#particle_instance.global_position = global_position
+	if particle_instance == null:
+		print("This worked. Wheres my sparks???")
+		var instance = particle_scene.instantiate()
+		get_tree().root.add_child(instance)
+		instance.global_position = global_position + Vector2(0, 200)
 func _on_body_exited(_body):
 	if particle_instance != null:
 		particle_instance.queue_free()
