7243910f87f069d5c9782162d44d059d7e0e6631
davididev <davidwaggener@gmail.com>
2024-07-23 13:45:56 -0400

    Added touch screen controls to Android
    Also made player and HUD a "prefab" (I know that's not what they call it in Godot)

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,6 +1,15 @@
 extends CanvasLayer
 var nuts = 0
 var coins = 0
+static var Is_Mobile = false;
+
+func _enter_tree():
+	var osName = OS.get_name();
+	if osName == "Android":
+		Is_Mobile = true;
+	else:
+		Is_Mobile = false;
+
 
 func _process(_delta):
 	$CoinCounter/CoinCount.text = str(coins)
